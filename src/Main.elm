4de4a6d925b07646f203be5693b300e9e796482a
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2026-02-03 00:38:04 +0100

    cleanup

--- src/Main.elm
+++ src/Main.elm
@@ -12,7 +12,6 @@ port beatClick : String -> Cmd msg
 
 type alias Metronome =
     { bpm : Float
-    , timeSignature : TimeSignature
     , subdivision : Subdivision
     , active : Bool
     }
@@ -24,57 +23,15 @@ type alias Subdivision =
     }
 
 
-type alias TimeSignature =
-    { numerator : Int
-    , denominator : Int
-    }
-
-
 type alias Model =
     { metronome : Metronome
     }
-
-
-allSubdivisions : List Subdivision
-allSubdivisions =
-    [ { name = "Straight Quarters", groups = [ 1, 1, 1, 1 ] }
-    , { name = "8th Subdivision", groups = [ 1, 1, 1, 1, 1, 1, 1, 1 ] }
-    , { name = "Straight Quarters", groups = [ 1, 1, 1 ] }
-    , { name = "8th Subdivision", groups = [ 2, 2, 2 ] }
-    , { name = "2+2+3", groups = [ 2, 2, 3 ] }
-    , { name = "3+2+2", groups = [ 3, 2, 2 ] }
-    , { name = "2+3+2", groups = [ 2, 3, 2 ] }
-    , { name = "2+3", groups = [ 2, 3 ] }
-    , { name = "3+2", groups = [ 3, 2 ] }
-    , { name = "Compound Meter (2x3)", groups = [ 3, 3 ] }
-    , { name = "Straight Eighths", groups = [ 1, 1, 1, 1, 1, 1 ] }
-    , { name = "Compound Meter (3x3)", groups = [ 3, 3, 3 ] }
-    , { name = "3+2", groups = [ 3, 2 ] }
-    , { name = "2+3", groups = [ 2, 3 ] }
-    , { name = "Straight Quarters", groups = [ 1, 1, 1, 1, 1 ] }
-    , { name = "Waltz Double (3+3)", groups = [ 3, 3 ] }
-    , { name = "Straight Quarters", groups = [ 1, 1, 1, 1, 1, 1 ] }
-    ]
-
-
-allTimeSigs : List TimeSignature
-allTimeSigs =
-    [ { numerator = 4, denominator = 4 }
-    , { numerator = 3, denominator = 4 }
-    , { numerator = 7, denominator = 8 }
-    , { numerator = 5, denominator = 8 }
-    , { numerator = 6, denominator = 8 }
-    , { numerator = 9, denominator = 8 }
-    , { numerator = 5, denominator = 4 }
-    , { numerator = 6, denominator = 4 }
-    ]
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
     ( { metronome =
             { bpm = 120
-            , timeSignature = { numerator = 4, denominator = 4 }
             , subdivision = { name = "Straight Quarters", groups = [ 1, 1, 1, 1 ] }
             , active = False
             }
@@ -87,7 +44,6 @@ type Msg
     = SetBpm Float
     | Start
     | Stop
-    | SetTimeSignature TimeSignature
     | Beat
 
 
@@ -111,11 +67,6 @@ setBpm metronome bpm =
     { metronome | bpm = bpm }
 
 
-setTimeSignature : Metronome -> TimeSignature -> Metronome
-setTimeSignature metronome timeSignature =
-    { metronome | timeSignature = timeSignature }
-
-
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
@@ -130,9 +81,6 @@ update msg model =
 
         SetBpm newBpm ->
             ( { model | metronome = setBpm model.metronome newBpm }, Cmd.none )
-
-        SetTimeSignature timeSignature ->
-            ( { model | metronome = setTimeSignature model.metronome timeSignature }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -171,7 +119,7 @@ view model =
 viewBpmControl : Model -> Html Msg
 viewBpmControl model =
     div [ style "display" "flex", style "align-items" "center", style "justify-content" "center" ]
-        [ span [] [ text "BPM: " ]
+        [ span [] [ text ("BPM: " ++ String.fromFloat model.metronome.bpm) ]
         , inputSlider model.metronome.bpm
         ]
 
