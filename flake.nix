f41c23230c330ce7f8fac1c61eed289e365a275a
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2026-04-03 00:09:44 +0200

    pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -145,14 +145,15 @@
                     exec = "elm-test";
                   };
                 };
-                # git-hooks.hooks = {
-                #   nixfmt.enable = true;
-                #   prettier.enable = true;
-                #   elm-format.enable = true;
-                #   deadnix.enable = true;
-                #   statix.enable = true;
-                #   elm-review.enable = true;
-                # };
+                git-hooks.hooks = {
+                  nixfmt.enable = true;
+                  prettier.enable = true;
+                  prettier.package = pkgs.prettier;
+                  elm-format.enable = true;
+                  deadnix.enable = true;
+                  statix.enable = true;
+                  elm-review.enable = true;
+                };
               }
             ];
           };
